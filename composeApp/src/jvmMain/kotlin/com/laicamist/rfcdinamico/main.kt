100f9c4ebc95606fc041fff75850db69e5969712
Laicamist <iperezalbor@gmail.com>
2026-02-17 01:56:37 -0600

    RFC

--- composeApp/src/jvmMain/kotlin/com/laicamist/rfcdinamico/main.kt
+++ composeApp/src/jvmMain/kotlin/com/laicamist/rfcdinamico/main.kt
@@ -1,13 +1,24 @@
 package com.laicamist.rfcdinamico
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-
+import androidx.compose.ui.window.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.runtime.*
 fun main() = application {
+    val windowState = rememberWindowState(
+        size = DpSize(1920.dp, 1080.dp)
+    )
+
     Window(
         onCloseRequest = ::exitApplication,
-        title = "rfcdinamico",
+        title = "RFC Dinámico",
+        state = windowState,
     ) {
-        App()
+        MaterialTheme {
+            App()  // Tu Composable RFC
+        }
     }
 }
